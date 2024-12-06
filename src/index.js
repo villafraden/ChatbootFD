c10ab4b2da1f13069c32fbb5e1a1bf4e61602320
Nelson Villafrade <villafrade@gmail.com>
2024-12-06 14:50:51 -0500

    ajuste del puerto 300 al 8080

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const apiruta = require("./routes/ruta");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
