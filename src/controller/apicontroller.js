61baad90922a46b8f75bd62a192ec58516da6926
Nelson Villafrade <villafrade@gmail.com>
2024-12-09 11:24:16 -0500

    pruebas codigo Udemy

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,71 +1,46 @@
 const enviarmensaje = require("../service/apiservice");
+const verificar = (req, res) => {
 
+    try{
+        var tokenandercode = "ANDERCODENODEJSAPIMETA";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
 
-const verificar=(req, res)=>{
-
-try {
-
-    var tokenserviclientefdses = "FDSESSERVICLI"
-    var token = req.query["hub.verify_token"];
-    var challenge = req.query["hub.challenge"];
-
-    if(challenge != null && token != null && token == tokenserviclientefdses ){
-      res.send(challenge);
-    }else{
-      res.status(400).send();
-    }
-
-     res.send(challenge);
-     console.log(req);
+        if (challenge != null && token != null && token == tokenandercode){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
 
     }catch(e){
-      res.status(400).send();
-
+        res.status(400).send();
     }
-
- res.send("Verificado");
- console.log("Verificado Consola");
 
 }
 
-const recibir=(req, res)=>{
-  try {
-    var entry = (req.body["entry"])[0];
-    var changes = (entry["changes"])[0];
-    var value = changes["value"];
-    var objetoMensaje = value["messages"];
+const recibir = (req, res) => {
+    try{
+        var entry = (req.body["entry"])[0];
+        var changes = (entry["changes"])[0];
+        var value = changes["value"];
+        var objetoMensaje = value["messages"];
 
+        if (typeof objetoMensaje != "undefined"){
+            var messages = objetoMensaje[0];
 
-  if(typeof objetoMensaje != undefined)
-  {
-   
-    
-    var messages = objetoMensaje[0];
-    var  texto = messages["text"]["body"];
-    var number = messages["from"];
+            var texto = messages["text"]["body"];
+            var numero = messages["from"];
 
-    console.log(entry);
-    console.log(changes);
-    console.log(objetoMensaje);
-
-    /*console.log("Enviado desde: "+ number + "El texto es el siguiente: "+ texto);*/
-    enviarmensaje.EnviarMensajeWhastapp(texto, number);
-
-  }
-   
-    res.send("EVENT_RECEIVED");
-
+            enviarmensaje.EnviarMensajeWhastpapp(texto,numero);
+        }
+        res.send("EVENT_RECEIVED");
     }catch(e){
-      console.log(e);
-     res.send("EVENT_RECEIVED");
-
-
+        console.log(e);
+        res.send("EVENT_RECEIVED");
     }
-  
-  console.log("Recibido Consola");
 }
 
 module.exports = {
-  verificar, 
-  recibir
+    verificar,
+    recibir
 }
