dca847b3c8661dd75e6f1aae3f5d3a4f4d023c37
Nelson Villafrade <villafrade@gmail.com>
2024-12-09 12:11:00 -0500

    codigo  udemy para pruebas

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-
 app.use("/api",apiruta);
 app.listen(PORT,()=>{
   console.log("Hola, V1El puerto es: " + PORT);
